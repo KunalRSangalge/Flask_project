8fe07e14bc2b80d94cc55f8a778c2d19887ea05f
KunalRS <kunal.sangalge@gmail.com>
2026-05-28 15:21:45 +0530

    fixed login issues

--- Flask_Market/market/__init__.py
+++ Flask_Market/market/__init__.py
@@ -9,5 +9,6 @@ app.config['SECRET_KEY'] = 'cfc097cbfff76668a99ec072'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
 from market import routes
